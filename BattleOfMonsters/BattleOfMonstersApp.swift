e7bb317795a4fff0ad18ebbfa47da9d7ee08cc6d
Francisco Cordoba <pako.cordoba10@gmail.com>
2024-09-17 16:22:49 -0600

    Project created

---
+++ BattleOfMonsters/BattleOfMonstersApp.swift
@@ -0,0 +1,17 @@
+//
+//  BattleOfMonstersApp.swift
+//  BattleOfMonsters
+//
+//  Created by Francisco Cordoba on 17/9/24.
+//
+
+import SwiftUI
+
+@main
+struct BattleOfMonstersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
